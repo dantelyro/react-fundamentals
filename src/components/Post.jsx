5d34a146646a8205facee37836478db93cc39519
Dante <beyondmind9@gmail.com>
2024-07-09 13:25:18 -0300

    button to delete comment

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -35,6 +35,11 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText(event.target.value)
   }
 
+  function deleteComment(commentToDelete){
+    const commentsWithoutDeletedOne = comment.filter((val) => val !== commentToDelete)
+    setComment(commentsWithoutDeletedOne)
+  }
+
   return (
     <article className={styles.post}>
       <header>
@@ -55,9 +60,9 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.content}>
       {content.map(line => {
           if (line.type === 'paragraph') {
-            return <p>{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
           } else if (line.type === 'link') {
-            return <p><a href="#">{line.content}</a></p>
+            return <p key={line.content}><a href="#">{line.content}</a></p>
           }
         })}
       </div>
@@ -79,7 +84,13 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comment.map(comment => {
-         return <Comment content={comment}/>
+          return (
+            <Comment 
+              key={comment} 
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          )
         })}
       </div>
     </article>
